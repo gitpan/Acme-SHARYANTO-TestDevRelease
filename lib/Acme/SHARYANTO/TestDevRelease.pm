8f4f187bd300f1ed6cb5d79ac878dc828b3bd7ee
Steven Haryanto <stevenharyanto@gmail.com>
2013-01-31 01:45:33 +0000

    Import of SHARYANTO/Acme-SHARYANTO-TestDevRelease-0.01 from CPAN.
    gitpan-cpan-distribution: Acme-SHARYANTO-TestDevRelease
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Acme-SHARYANTO-TestDevRelease-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Acme/SHARYANTO/TestDevRelease.pm
@@ -0,0 +1,31 @@
+package Acme::SHARYANTO::TestDevRelease;
+
+our $VERSION = '0.01'; # VERSION
+
+1;
+# ABSTRACT: Test dev release
+
+__END__
+=pod
+
+=head1 NAME
+
+Acme::SHARYANTO::TestDevRelease - Test dev release
+
+=head1 VERSION
+
+version 0.01
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
