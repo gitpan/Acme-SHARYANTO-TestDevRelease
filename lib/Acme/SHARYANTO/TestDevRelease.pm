656b1710db541dea5a4d10b595a463780500128a
Steven Haryanto <stevenharyanto@gmail.com>
2013-01-31 02:01:35 +0000

    Import of SHARYANTO/Acme-SHARYANTO-TestDevRelease-0.01_02 from CPAN.
    gitpan-cpan-distribution: Acme-SHARYANTO-TestDevRelease
    gitpan-cpan-version:      0.01_02
    gitpan-cpan-path:         SHARYANTO/Acme-SHARYANTO-TestDevRelease-0.01_02.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     developer

--- lib/Acme/SHARYANTO/TestDevRelease.pm
+++ lib/Acme/SHARYANTO/TestDevRelease.pm
@@ -1,6 +1,6 @@
 package Acme::SHARYANTO::TestDevRelease;
 
-our $VERSION = '0.02'; # VERSION
+our $VERSION = '0.01_02'; # VERSION
 
 1;
 # ABSTRACT: Test dev release
@@ -14,7 +14,7 @@ Acme::SHARYANTO::TestDevRelease - Test dev release
 
 =head1 VERSION
 
-version 0.02
+version 0.01_02
 
 =head1 AUTHOR
 
